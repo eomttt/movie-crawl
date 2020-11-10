160f0ba7b699c1cfcd8edfc6c83963991830a4c8
DESKTOP-IIEK8B4\shy <hachu.developer@gmail.com>
2020-11-10 23:22:47 +0900

    megabox api 호출하여 image가져오기

--- controller/movie-megabox.controller.js
+++ controller/movie-megabox.controller.js
@@ -1,5 +1,6 @@
 // const puppeteer = require('puppeteer');
 const chromium = require('chrome-aws-lambda');
+const fetch = require("node-fetch");
 
 const MEGA_HOST_URL = 'https://www.megabox.co.kr';
 const MEGA_GET_BY_REGION = 'https://www.megabox.co.kr/theater/list';
@@ -72,34 +73,47 @@ const getTheatersByRegions = async (regionIndex = GANGWON_INDEX) => {
     }
 };
 
-const getImageUrl = async (link) => {
-    const browser = await chromium.puppeteer.launch({
-        args: [...chromium.args],
-        defaultViewport: chromium.defaultViewport,
-        executablePath: await chromium.executablePath,
-        headless: chromium.headless,
-    });
-    const MEGA_BOX_IMAGE_URL = `https://www.megabox.co.kr${link}`;
-    const page = await browser.newPage();;
-    console.log('link : ', `${MEGA_BOX_IMAGE_URL}`);
-    try {
-        await page.goto(`${MEGA_BOX_IMAGE_URL}`, {
-            waitUntil: 'load',
-            // Remove the timeout
-            timeout: 0
-        });
-        await page.waitFor(1000);
-        const imageUrl = await page.evaluate(() => {
-            return document.querySelector('#schdlContainer > #contents > .movie-detail-page > .movie-detail-cont > .poster > .wrap > img').getAttribute('src');
-        });
-        console.log('imageUrl : ', imageUrl);
-        return imageUrl;
+// const getImageUrl = async (link) => {
+//     const browser = await chromium.puppeteer.launch({
+//         args: [...chromium.args],
+//         defaultViewport: chromium.defaultViewport,
+//         executablePath: await chromium.executablePath,
+//         headless: chromium.headless,
+//     });
+//     const MEGA_BOX_IMAGE_URL = `https://www.megabox.co.kr${link}`;
+//     const page = await browser.newPage();;
+//     console.log('link : ', `${MEGA_BOX_IMAGE_URL}`);
+//     try {
+//         await page.goto(`${MEGA_BOX_IMAGE_URL}`, {
+//             waitUntil: 'load',
+//             // Remove the timeout
+//             timeout: 0
+//         });
+//         await page.waitFor(1000);
+//         const imageUrl = await page.evaluate(() => {
+//             return document.querySelector('#schdlContainer > #contents > .movie-detail-page > .movie-detail-cont > .poster > .wrap > img').getAttribute('src');
+//         });
+//         console.log('imageUrl : ', imageUrl);
+//         return imageUrl;
 
-    } catch (error) {
-        console.log('Get image URL error MEGA', error);
-    } finally {
-        browser.close();
-    }
+//     } catch (error) {
+//         console.log('Get image URL error MEGA', error);
+//     } finally {
+//         browser.close();
+//     }
+// }
+
+const getImageUrl = async (imageNumber) => {
+	const response = await fetch(`https://www.megabox.co.kr/on/oh/oha/Movie/selectOneLnList.do?currentPage=1&movieNo=${imageNumber}`);
+    const res = await response.json().then(
+        data => {
+            if (!!data && !!data.list && !!data.list[0] && !!data.list[0].movieFilePath) {
+                const imagesvrUrl = data.imgSvrUrl;
+                const movieFilePath = !!data.list[0].movieFilePath ? data.list[0].movieFilePath : '';
+                console.log(`${imagesvrUrl}${movieFilePath}`);
+                return `${imagesvrUrl}${movieFilePath}`;
+            }
+    }, setTimeout(()=>1000));
 }
 
 const getTimeTable = async (link = MOCK_THEATER_INFO.link) => {
@@ -111,6 +125,7 @@ const getTimeTable = async (link = MOCK_THEATER_INFO.link) => {
     });
     const page = await browser.newPage();;
     try {
+        console.log(`${MEGA_HOST_URL}${link}`);
         await page.goto(`${MEGA_HOST_URL}${link}`, {
             waitUntil: 'load',
             // Remove the timeout
@@ -121,7 +136,7 @@ const getTimeTable = async (link = MOCK_THEATER_INFO.link) => {
         const movieItems = await page.evaluate(() => {
             const items = Array.from(document.querySelectorAll('.reserve > .theater-list'));
             return items.map((item) => {
-                const imageNumber = item.querySelector('.theater-tit > p:nth-child(2) > a').getAttribute('href');
+                const imageNumber = item.querySelector('.theater-tit > p:nth-child(2) > a').getAttribute('href').split('=')[1];
                 const title = item.querySelector('.theater-tit > p:nth-child(2)').innerText;
                 const timeTables = Array.from(item.querySelectorAll('.theater-type-box'));
                 const timeInfo = timeTables.map((timeTable) => {
@@ -145,16 +160,23 @@ const getTimeTable = async (link = MOCK_THEATER_INFO.link) => {
                 };
             });
         });
-
         return movieItems.map((movieItem) => {
+            let final = '';
+            getImageUrl(movieItem.imageNumber).then(x=>{
+                if (!!x) {
+                    final = x;
+                }
+            })
+            setTimeout(()=>10000);
             return {
                 title: movieItem.title,
                 timeInfo: movieItem.timeInfo.reduce((acc, cur) => {
                     return [...acc, ...cur];
                 }, []),
-                // imageUrl: getImageUrl(movieItem.imageNumber)
+                imageUrl: final
             };
         })
+        
     } catch (error) {
         console.log('Get theater timetable error MEGA', error);
     } finally {
