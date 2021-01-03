fe3e69645a4c5acc86fffa011ef07a907ae5d609
eomttt <hyunt0413@gmail.com>
2021-01-03 16:11:47 +0900

    Megabox movieFilePath 수정

--- controller/movie-megabox.controller.js
+++ controller/movie-megabox.controller.js
@@ -76,14 +76,13 @@ const getTheatersByRegions = async (regionIndex = GANGWON_INDEX) => {
 const getImage = async (imageNumber) => {
 	const response = await fetch(`https://www.megabox.co.kr/on/oh/oha/Movie/selectOneLnList.do?currentPage=1&movieNo=${imageNumber}`);
     const data = await response.json();
-    console.log("CCCC", data)
     // TODO: Node 14 부터 optional chaining 적용 됨.
     // Lambda 는 12 까지만 지원함 ㅠㅠ 고로 못씀...
     // https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Operators/Optional_chaining
 
     if (data) {
         const { imgSvrUrl, list } = data;
-        if (list) {
+        if (list && list[0].movieFilePath) {
             let path = list[0].movieFilePath;
             list.some((item) => {
                 if (item.movieFilePath) {
