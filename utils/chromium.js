be84d71434c09813a5002ed2df788d0a5e75a87a
eomttt <hyunt0413@gmail.com>
2023-03-04 23:45:22 +0900

    :bookmark: Node version 14 -> 16 (https://velog.io/@shelly/ubuntu%EC%97%90%EC%84%9C-puppeteer-%EC%8B%A4%ED%96%89-%EC%98%A4%EB%A5%98)

--- utils/chromium.js
+++ utils/chromium.js
@@ -2,7 +2,7 @@ import chromium from "chrome-aws-lambda";
 
 export const launchChromium = async () => {
   const browser = await chromium.puppeteer.launch({
-    args: chromium.args,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
     defaultViewport: chromium.defaultViewport,
     executablePath: await chromium.executablePath,
     headless: chromium.headless,
